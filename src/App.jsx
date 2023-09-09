2526bf8787551928826cab4d543124de3c02a9ff
ShohKhan-dev <shoh.hamraqulov@gmail.com>
2023-09-09 21:45:54 +0300

    adding chatroom

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import Create from './components/Create';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CategoryList from "./components/Words/Categories.jsx";
 import DialogsList from "./components/Dialogs/DialogsList";
-import Chat from "./components/Dialogs/Chat";
-
+import ChatRoom from "./components/Dialogs/Chat";
 
 function App() {
   return (
@@ -16,9 +15,10 @@ function App() {
         <Routes>
           <Route path="/" exact Component={Home} />
           <Route path="/dialogs" Component={DialogsList} />
-          <Route path="/chat/:id" Component={Chat}/>
+          <Route path="/chat/:id" Component={ChatRoom}/>
           <Route path="/create" Component={Create} />
           <Route path="/word" Component={CategoryList} />
+
         </Routes>
       </div>
     </Router>
--- src/components/Dialogs/Chat.jsx
+++ src/components/Dialogs/Chat.jsx
@@ -1,7 +1,9 @@
 import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+
 import items from "./Items";
 
-const Chat = () => {
+const ChatRoom = () => {
 
     // Use the useParams hook to get the item ID from the URL
   const { id } = useParams();
@@ -12,49 +14,104 @@ const Chat = () => {
   if (!selectedItem) {
     return <div>Item not found</div>;
   }
+  const [messages, setMessages] = useState([]);
+  const [isTyping, setIsTyping] = useState(true);
+  const [optionsVisible, setOptionsVisible] = useState(true);
+
+  useEffect(() => {
+    const typingTimer = setTimeout(() => {
+      setIsTyping(false);
+
+      const computerMessage = {
+        id: 'computer',
+        text: 'Hello, how can I help you?',
+      };
+
+      setMessages((prevMessages) => [...prevMessages, computerMessage]);
+    }, 1500);
+
+    return () => clearTimeout(typingTimer);
+  }, []);
+
+  const handleOptionClick = (optionText) => {
+    // Display the user's selected option as a message
+    const userMessage = {
+      id: 'user',
+      text: optionText,
+    };
+
+    // Add the user's message to the existing messages
+    setMessages((prevMessages) => [...prevMessages, userMessage]);
+
+    // Simulate computer typing
+    setIsTyping(true);
+
+    // Simulate a delay before the computer responds
+    setTimeout(() => {
+      setIsTyping(false);
+
+      // Simulate computer's response
+      const computerMessage = {
+        id: 'computer',
+        text: 'Thank you for choosing: ' + optionText + '. How can I assist you further?',
+      };
+
+      // Add the computer's message to the existing messages
+      setMessages((prevMessages) => [...prevMessages, computerMessage]);
+
+      // Show options again
+      setOptionsVisible(true);
+    }, 1500);
+
+    // Hide options temporarily
+    setOptionsVisible(false);
+  };
+
+  console.log(messages);
 
   return (
-        <div className="lg:col-span-2 lg:block ">
-      <div className="w-full">
-        <div className="p-3 bg-white border-b border-gray-200 dark:bg-gray-900 dark:border-gray-700 text-white">
-            {selectedItem.title}
-        </div>
+    <div className="bg-gray-200 p-4 h-96">
+      <h1>{selectedItem.title}</h1>
+      {/* Render chat messages */}
+      <div className="mb-4">
+        {messages.map((message, index) => (
+          <div
+            key={index}
+            className={`${message.id === 'computer' ? 'text-left' : 'text-right'} mb-2`}
+          >
+            {message.text}
+          </div>
+        ))}
+      </div>
 
-        <div className="relative w-full p-6 overflow-y-auto h-[30rem] bg-white border-b border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-          <ul className="space-y-2">
-            
-              <div className="text-white text-left">
-                Some MEssage
-              </div>
-              <div  className="text-white text-right">
-                Another message
-              </div>
-              <div  className="text-white text-left">
-                Come cools message or what?
-              </div>
-          
-          </ul>
-        </div>
+      {/* Render typing indicator */}
+      {isTyping && <div className="text-left animate-pulse">...</div>}
 
-        <form>
-        <div className="flex items-center justify-between w-full p-3 bg-white border-b border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-
-          <input
-            type="text"
-            placeholder="Write a message"
-            className="block w-full py-2 pl-4 mx-3 outline-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            name="message"
-            required
-          />
-          <button type="submit" className="text-white">
-            
-             Submit
+      {/* Render user options */}
+      {optionsVisible && (
+        <div className="grid grid-cols-3 gap-4">
+          <button
+            className="bg-blue-500 text-white py-2 px-4 rounded"
+            onClick={() => handleOptionClick('Option 1')}
+          >
+            Option 1
+          </button>
+          <button
+            className="bg-blue-500 text-white py-2 px-4 rounded"
+            onClick={() => handleOptionClick('Option 2')}
+          >
+            Option 2
+          </button>
+          <button
+            className="bg-blue-500 text-white py-2 px-4 rounded"
+            onClick={() => handleOptionClick('Option 3')}
+          >
+            Option 3
           </button>
         </div>
-      </form>
-      </div>
+      )}
     </div>
   );
 };
 
-export default Chat;
+export default ChatRoom;
