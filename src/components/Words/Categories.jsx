82369db16b2717d4ee477f8d57aebffbd9eb2e4d
e-maks <matantsev0@gmail.com>
2023-09-09 22:44:57 +0300

    Beautify categories

--- src/components/Words/Categories.jsx
+++ src/components/Words/Categories.jsx
@@ -41,14 +41,17 @@ const CategoryList = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             {categories.map((category) => (
                 <Link to={`/category/${category.id}`} key={category.id}>
-                    <div className="bg-white p-4 rounded-lg shadow-md hover:shadow-lg cursor-pointer">
-                        <h2 className="text-xl font-semibold">{category.title}</h2>
-                        <p className="mt-2">{category.description}</p>
+                    <div className="bg-white p-4 rounded-lg shadow-md hover:shadow-lg h-full">
+                        <div className="h-24    "> {/* Fixed height for title */}
+                            <h2 className="text-xl font-semibold line-clamp-3">{category.title}</h2>
+                        </div>
+                        <div className="h-16 mt-2"> {/* Fixed height for description */}
+                            <p className="line-clamp-2">{category.description}</p>
+                        </div>
                     </div>
                 </Link>
             ))}
         </div>
     );
 };
-
 export default CategoryList;
