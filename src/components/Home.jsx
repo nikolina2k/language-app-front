52a5783207cd66a58a16d4e0d9871da5699a7e09
Khabib <k.khaysadykov@innopolis.university>
2023-09-09 20:06:54 +0300

    Add Home head

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,30 @@
+import streakIcon from "../assets/streak_icon.svg";
+
 const Home = () => {
-  return <div>User profile</div>;
+    return (
+        <div>
+            <div
+                className="h-screen py-10 px-8"
+                style={{ backgroundColor: "#87786D", color: "white" }}
+            >
+                <div className="flex justify-between">
+                    <span style={{ fontSize: "64px" }}>Максим Матанцев</span>
+                    <div
+                        className="flex items-center justify-center"
+                        style={{ display: "flex" }}
+                    >
+                        <img src={streakIcon} alt="streak icon" />
+                        <span style={{ fontSize: "36px" }}>28</span>
+                    </div>
+                </div>
+                <div className="flex flex-col items-start">
+                    <p style={{ fontSize: "36px" }}>Lvl 54</p>
+                    <div className="progres"></div>
+                    <p style={{ fontSize: "18px" }}>120/250 XP until the next level</p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
